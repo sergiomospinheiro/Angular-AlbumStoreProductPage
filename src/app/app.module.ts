64b2eabc88bc54fb6bd749966408fc2264d0a31d
CORP\smospinh <sergio-miguel.mos-pinheiro@altran.com>
2022-05-10 16:50:59 +0100

    sasfgsad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const appRoutes[]: Routes = [
+const appRoutes: Routes = [
     { path: 'products', component: ProductListComponent}
 ];
 
