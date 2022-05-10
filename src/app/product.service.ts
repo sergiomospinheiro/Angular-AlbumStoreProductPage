2df5a21b24ef42845151802483964f3de18bde49
CORP\smospinh <sergio-miguel.mos-pinheiro@altran.com>
2022-05-10 11:56:56 +0100

    4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,11 +10,7 @@ export class ProductService {
   constructor( private _http: Http) { }
 
   getAlbum(id:number) { 
-      return this._http.get(this._albumUrl).map(res: Response => {
-          return res.json());
-
-      } 
-     
+      return this._http.get(this._albumUrl).map((response) => response.json());  
 }
 }
 
